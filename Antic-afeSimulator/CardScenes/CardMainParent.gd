a6b789237c5ba45dc994e373602c0f1db30ef9de
BennyDent <147953580+BennyDent@users.noreply.github.com>
2025-07-28 05:06:09 +0300

    28_07.1

--- Antic-afeSimulator/CardScenes/CardMainParent.gd
+++ Antic-afeSimulator/CardScenes/CardMainParent.gd
@@ -1,16 +1,31 @@
 extends Node2D
+signal set_draggable
+signal move_card
 var is_change_y: bool = false;
+var start_position_y: float = 0-scale.y/4.00;
+var start_position_x: float;
+var hand_index: int;
+var rotation_undraggable_position: float;
+var x_undraggable_position: float;
 var y_destination: float;
 var rotation_destination: float;
 var is_rotation: float = false;
-var is_draggable: bool = true:
+var is_moved: bool = false;
+var is_played: bool = false;
+var is_change_x: bool = false;
+var x_destination: float;
+var is_draggable = true;
+var is_dragged: bool = true:
+
 	get:
-		return is_draggable;
+		return is_dragged;
 	set(input):
-		is_draggable = input;
+		is_dragged = input;
 		if(input):
-			get_draggable_position();
-			print(1);
+			if(is_draggable):
+				get_draggable_position();
+				
+			
 		else:
 			get_not_draggable_position();
 			
@@ -19,41 +34,91 @@ var is_draggable: bool = true:
 
 func _process(delta):
 	if(is_change_y):
-		if(y_destination> global_position.y):
-			global_position.y = global_position.y+ (y_destination-global_position.y)*(delta*5);
+		if(y_destination> position.y):
+			position.y = position.y+ (y_destination-position.y)*(delta*5);
 		else:
-			global_position.y = global_position.y- (global_position.y- y_destination)*(delta*5);
-		if(global_position.y == y_destination):
+			position.y = position.y- (position.y- y_destination)*(delta*5);
+		if(position.y == y_destination):
 			is_change_y= false;
+			
+	if is_change_x:
+		if(x_destination> global_position.x):
+			global_position.x = global_position.x+ (x_destination-global_position.x)*(delta*5);
+		else:
+			global_position.x = global_position.x- (global_position.x- x_destination)*(delta*5);
+		if(global_position.x == x_destination):
+			is_change_x= false;
 	if(is_rotation):
 		if(rotation_destination > rotation):
-			global_rotation = global_rotation + (rotation_destination-global_rotation)*(delta*5)
+			rotation = rotation + (rotation_destination-rotation)*(delta*5)
 		else:
-			global_rotation = global_rotation - (global_rotation - rotation_destination)*(delta*5);
-		if(global_rotation==rotation_destination):
+			rotation = rotation - (rotation - rotation_destination)*(delta*5);
+		if(rotation==rotation_destination):
 			is_rotation = false;
+	if is_dragged and Input.is_action_just_pressed("left_mouse"):
+		is_moved= true;
+		set_draggable.emit(hand_index, false);
+	if is_moved:
+		if Input.is_action_pressed("left_mouse"):
+			global_position = get_global_mouse_position();
+			if start_position_x-global_position.x <0 or start_position_x-global_position.x >scale.x:
+				move_card.emit(hand_index+1);
+		if Input.is_action_just_released("left_mouse"):
+			is_moved = false;
+			set_draggable.emit(hand_index, true);
+			if is_played:
+				pass
+			else:
+				is_dragged= false;
+					
+
+
+func _on_area_exited():
+	is_played== true;
+
+
 
 func _ready():
-	global_position.y =  get_viewport().get_visible_rect().size.y-scale.y/2;
+	start_position_y = start_position_y - float(hand_index)*10.00;
+	position.y = start_position_y;
 
 func start_y_motion(input):
 	y_destination = input;
 	print(y_destination);
 	is_change_y = true;
+	
+	
+func start_x_motion(input):
+	x_destination = input;
+	is_change_x = true;
 
 func start_rotation_motion(input):
 	rotation_destination = input;
 	is_rotation=true;
 
 func get_draggable_position():
-	var  bottom_position = get_viewport().get_visible_rect().size;
-	start_y_motion( bottom_position.y - scale.y/2);
 	
-	start_rotation_motion(0.0)
+	var  bottom_position = get_viewport().get_visible_rect().size;
+	start_y_motion( start_position_y -20.00 );
+	z_index = 13;
+	start_rotation_motion(0.0);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func get_not_draggable_position():
+	
 	var new_global_position= get_viewport().get_visible_rect().size;
-	
-	
-	start_rotation_motion(0.3);
-	start_y_motion(new_global_position.y);
+	z_index = hand_index;
+	start_rotation_motion(rotation_undraggable_position);
+	start_y_motion(start_position_y);
+	if(position.x !=start_position_x):
+		start_x_motion(start_position_x);
+
+
+func _on_area_2d_mouse_entered():
+	is_dragged = true;
+	print(is_draggable);# Replace with function body.
+
+
+func _on_area_2d_mouse_exited():
+	is_dragged=false;
+	print(is_draggable);
+	 # Replace with function body.
--- Antic-afeSimulator/collision_shape_2d.gd
+++ Antic-afeSimulator/collision_shape_2d.gd
@@ -1,10 +1,55 @@
 extends CollisionShape2D
 var positions_array = []
-@export  var first = 200.00;
+@export var cards_scale =150;
+var dictionary = {1: 5,2: 5, 3:5, 4:4, 5:4, 6: 3,}
+var rotations_positions = [-0.040, -0.030,  -0.020, -0.010, -0.0, 0.010, 0.020, 0.030, 0.040, 0.050  ]
+@export  var first = 100.00;
 @export var shift = 50.00;
+
+
+func ReturnHandIndex(hand_index):
+
+	var result = get_children().filter(func(a): return a.hand_index==hand_index);
+	if result.size()>0:
+		return result[0]
+	else:
+		return;
+
 func _ready():
 	for i in range(12):
-		positions_array.append(first + (float((i+1))*shift)) ;
+		
+		positions_array.append((float((i+1)*cards_scale)));
 	var results = get_children();
+	var start = dictionary[results.size()]
 	for  i in range(get_children().size()):
-		results[i].global_position.x = positions_array[i];
+		
+		var to_change = results[i]
+		get_parent().area_exited.connect(to_change._on_area_exited);
+		to_change.set_draggable.connect(_on_emit_signal);
+		to_change.move_card.connect(_on_card_move);
+		to_change.start_position_x = positions_array[start+i];
+		to_change.global_position.x = positions_array[start+i];
+		to_change.hand_index = start+i;
+		to_change.rotation =  rotations_positions[start+i];
+	
+
+func _on_card_move(hand_index):
+	var to_move = ReturnHandIndex(hand_index);
+	
+	if to_move != null:
+		to_move.start_x_motion(positions_array[hand_index-1])
+		to_move.start_position_x = positions_array[hand_index-1];
+		to_move.hand_index = hand_index-1;
+		print(to_move.hand_index);
+	var moved = ReturnHandIndex(hand_index-1);
+	if moved != null:
+		moved.start_position_x = positions_array[hand_index];
+		moved.hand_index = hand_index;
+
+func _on_emit_signal(hand_index, is_dragged):
+	var results = get_children();
+	
+	for i in range(results.size()):
+		if results[i].hand_index != hand_index:
+			results[i].is_draggable = is_dragged;
+		
